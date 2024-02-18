bb786f7111168342cd40bf626f140a61e82702bf
mulzahrian <mulzahrianone@gmail.com>
2024-02-18 20:46:40 +0700

    add room management2

--- application/controllers/Management.php
+++ application/controllers/Management.php
@@ -14,11 +14,21 @@ class Management extends CI_Controller
         $data['title'] = 'Management Room';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('management/index', $data);
-        $this->load->view('templates/footer');
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('management/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">New menu added!</div>');
+            redirect('menu');
+        }
     }
 
 
